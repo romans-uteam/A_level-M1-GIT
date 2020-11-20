cdcbc8dd7a5e6ea5ccac2191d929b7ca9db1a90f
Roman Shuvalov <romans@uteam.co.il>
2020-11-20 21:11:54 +0200

    Hello World added

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
