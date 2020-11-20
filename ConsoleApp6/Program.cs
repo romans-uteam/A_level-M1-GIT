6948e666655f31a698a34edc0152d5869f2cfd10
Roman Shuvalov <romans@uteam.co.il>
2020-11-20 21:31:17 +0200

    Solved Excercise 1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,11 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Console.Write("Your Name: ");
+            string input = File.ReadAllText("INPUT.TXT");
 
-            string name = Console.ReadLine();
+            string output = input;
 
-            Console.WriteLine("Hello, {0}!", name);
-
-            Console.ReadKey();
+            File.WriteAllText("OUTPUT.TXT", output);
         }
     }
 }
